026c9c18ff38d159ab7438a9b084830c80dd9a5a
ParaschivGeorge <georgegmtt@gmail.com>
2019-05-12 22:54:45 +0300

    projects view and delete

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../services/projects.service';
+import { Project } from '../models/project';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  projects: Project[] = [];
+
+  constructor(private projectsService: ProjectsService) { }
+
+  // TODO: add create method
 
   ngOnInit() {
+    this.getProjects();
+  }
+
+  getProjects() {
+    // TODO provide user company id
+    this.projectsService.getProjects(null, null).subscribe(
+      projects => {
+        this.projects = projects;
+        console.log(projects);
+      }
+    );
+  }
+
+  deleteProject(id: number) {
+    this.projectsService.deleteProject(id).subscribe(
+      data => {
+        console.log(data);
+        this.getProjects();
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
 }
