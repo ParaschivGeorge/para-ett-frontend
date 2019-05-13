54284ed0e2b2f02f5fa1efe1229b707f57959cfa
ParaschivGeorge <georgegmtt@gmail.com>
2019-05-13 22:12:20 +0300

    projects create and update

--- src/app/projects/project/project.component.ts
+++ src/app/projects/project/project.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from 'src/app/services/projects.service';
+import { ActivatedRoute } from '@angular/router';
+import { ErrorStateMatcher } from '@angular/material';
+import { FormControl, FormGroupDirective, NgForm, FormArray, FormGroup, Validators } from '@angular/forms';
+import { Project } from 'src/app/models/project';
+import { ProjectDto } from 'src/app/models/project-dto';
+import { User } from 'src/app/models/user';
 
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
@@ -7,9 +21,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  matcher = new MyErrorStateMatcher();
+  id: number;
+  project: Project;
+  projectEditForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    responsibleId: new FormControl(null, [Validators.required]),
+    users: new FormArray([])
+  });
+
+  constructor(private projectsService: ProjectsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params.id;
+    this.getProject(this.id);
+  }
+
+  getProject(id: number) {
+    this.projectsService.getProject(id).subscribe(
+      project => {
+        this.project = JSON.parse(JSON.stringify(project));
+        console.log(this.project);
+        delete project.companyId;
+        delete project.id;
+        const users: User[] = JSON.parse(JSON.stringify(project.users));
+        console.log(this.projectEditForm);
+        this.removeAllUsers();
+        project.users = [];
+        this.projectEditForm.setValue(project);
+        users.forEach(user => {
+          this.addUser(user.id);
+        });
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  get usersFormArray(): FormArray {
+    return this.projectEditForm.get('users') as FormArray;
+  }
+
+  addUser(userId: number) {
+    if (this.projectEditForm.valid) {
+      this.usersFormArray.push(new FormControl(userId, Validators.required));
+    }
+  }
+
+  removeUser(index: number) {
+    this.usersFormArray.removeAt(index);
+  }
+
+  onSubmit() {
+    console.log(this.projectEditForm.valid);
+    if (this.projectEditForm.valid) {
+      const projectDto = this.projectEditForm.value as ProjectDto;
+      projectDto.companyId = 1; // TODO set user company id
+      this.projectsService.updateProject(this.id, projectDto).subscribe(
+        project => {
+          console.log(project);
+          this.getProject(this.id);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  private removeAllUsers() {
+    for (let index = this.usersFormArray.length - 1; index >= 0; index--) {
+      this.removeUser(index);
+    }
   }
 
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectsService } from '../services/projects.service';
 import { Project } from '../models/project';
+import { FormArray, FormControl, Validators, FormGroupDirective, NgForm, FormGroup } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material';
+import { ProjectDto } from '../models/project-dto';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-projects',
@@ -10,10 +21,15 @@ import { Project } from '../models/project';
 export class ProjectsComponent implements OnInit {
 
   projects: Project[] = [];
+  matcher = new MyErrorStateMatcher();
+
+  projectCreateForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    responsibleId: new FormControl(null, [Validators.required]),
+    users: new FormArray([])
+  });
 
   constructor(private projectsService: ProjectsService) { }
-
-  // TODO: add create method
 
   ngOnInit() {
     this.getProjects();
@@ -39,6 +55,35 @@ export class ProjectsComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  get usersFormArray(): FormArray {
+    return this.projectCreateForm.get('users') as FormArray;
+  }
+
+  addUser() {
+    this.usersFormArray.push(new FormControl(null ,Validators.required));
+  }
+
+  removeUser() {
+    this.usersFormArray.removeAt(this.usersFormArray.length - 1);
+  }
+
+  onSubmit() {
+    console.log(this.projectCreateForm.valid);
+    if (this.projectCreateForm.valid) {
+      const projectDto = this.projectCreateForm.value as ProjectDto;
+      projectDto.companyId = 1; // TODO set user company id
+      this.projectsService.createProject(projectDto).subscribe(
+        data => {
+          console.log(data);
+          this.getProjects();
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
   }
 
 }
