e01122d49acf7de9246f7fba9ebf930159d1502e
ParaschivGeorge <georgegmtt@gmail.com>
2019-06-02 16:54:10 +0300

    activation component

--- src/app/activation/activation.component.ts
+++ src/app/activation/activation.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountActivationUserDto } from '../models/account-activation-user-dto';
 import { UsersService } from '../services/users.service';
 import { ErrorStateMatcher } from '@angular/material';
 import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup } from '@angular/forms';
 import { CompanyRegisterDto } from '../models/company-register-dto';
+import { DataHolderService } from '../services/data-holder.service';
+import { AuthService } from '../services/auth.service';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -23,6 +25,7 @@ export class ActivationComponent implements OnInit {
 
   code: string;
   email: string;
+  error: string = null;
   matcher = new MyErrorStateMatcher();
 
   passwordFormControl = new FormControl('', [
@@ -38,7 +41,12 @@ export class ActivationComponent implements OnInit {
     ])
   });
 
-  constructor(private route: ActivatedRoute, private usersService: UsersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private usersService: UsersService,
+    private dataHolderService: DataHolderService,
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -65,14 +73,34 @@ export class ActivationComponent implements OnInit {
         activationCode: this.code
       };
       console.log('AAUdto: ', accountActivationUserDto);
+      this.dataHolderService.loading = true;
       this.usersService.activateAccount(accountActivationUserDto).subscribe(
-        data => {
-          console.log(data);
+        user => {
+          console.log(user);
+          this.dataHolderService.user = user;
+          this.authService.login({email: user.email, password: accountActivationUserDto.password}).subscribe(
+            authResp => {
+              sessionStorage.setItem('token', authResp.token);
+              setTimeout(() => {
+                this.router.navigate(['timesheet-records']);
+              }, 1000);
+            },
+            error => {
+              this.dataHolderService.user = null;
+              this.error = 'Authentification failed! Redirecting home...';
+              setTimeout(() => {
+                this.router.navigate(['home']);
+              }, 1000);
+            }
+          );
         },
         error => {
           console.log(error);
+          this.error = 'Activation failed!';
         }
-      );
+      ).add(() => {
+        this.dataHolderService.loading = false;
+      });
     }
   }
 
